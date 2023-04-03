06c95016ea013399645979c94a03f90cf7a44cbf
ajhwan <129160008+ajhwan@users.noreply.github.com>
2023-04-03 09:59:45 +0900

    Create millis_Korea.ino

---
+++ millis_Korea.ino
@@ -0,0 +1,12 @@
+void setup() {
+  Serial.begin(115200);
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  unsigned long t_now = millis();
+  Serial.println(t_now);
+  // put your main code here, to run repeatedly:
+
+}
