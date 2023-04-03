2c0244a5f01f47563614845d6848c322852cb9c7
ajhwan <129160008+ajhwan@users.noreply.github.com>
2023-04-03 10:33:10 +0900

    Update millis_Korea.ino

--- millis_Korea.ino
+++ millis_Korea.ino
@@ -1,3 +1,10 @@
+unsigned long t1_prev = 0;
+const unsigned long t1_delay = 1000;
+
+unsigned long t2_prev = 0;
+const unsigned long t2_delay = 500;
+
+
 void setup() {
   Serial.begin(115200);
   // put your setup code here, to run once:
@@ -5,8 +12,16 @@ void setup() {
 }
 
 void loop() {
-  unsigned long t_now = millis();
-  Serial.println(t_now);
-  // put your main code here, to run repeatedly:
+  unsigned long t1_now = millis();
+  if (t1_now - t1_prev >= t1_delay){
+    t1_prev = t1_now;
+    Serial.println("t1");
+  }
+
+  unsigned long t2_now = millis();
+  if (t2_now - t2_prev >= t2_delay){
+    t2_prev = t2_now;
+    Serial.println("\tt2");
+  }
 
 }
